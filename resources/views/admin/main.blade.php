46b1f22274ccb48d2b7c5104a66f3f21a03a31c1
azmajz <53705217+azmajz@users.noreply.github.com>
2021-06-28 17:03:08 +0530

    Update main.blade.php

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -1,82 +1,85 @@
 <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>ADMIN {{config('app.name')}}</title>
-            <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
-            <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
-            <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    </head>
-    <body>
-        <!-- HEADER -->
-        <div id="header-admin">
-            <!-- container -->
-            <div class="container">
-                <!-- row -->
-                <div class="row">
-                    <!-- LOGO -->
-                    <div class="col-md-2">
-                        <a href="post.php"><img class="logo" src="{{asset('images/blogger-site.jpg')}}"></a>
-                    </div>
-                    <!-- /LOGO -->
-                      <!-- LOGO-Out -->
-                    <div class="col-md-offset-9  col-md-1">
-                        <form action="{{route('logout')}}" method="post">
-                            @csrf
-                            <button class="btn btn-primary admin-logout" type="submit">Logout</button>
-                        </form>
-                        {{-- <a href="logout.php" class="" >logout</a> --}}
-                    </div>
-                    <!-- /LOGO-Out -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ADMIN {{config('app.name')}}</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+
+<body>
+    <!-- HEADER -->
+    <div id="header-admin">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- LOGO -->
+                <div class="col-md-2">
+                    <a href="post.php"><img class="logo" src="{{asset('images/blogger-site.jpg')}}"></a>
                 </div>
+                <!-- /LOGO -->
+                <!-- LOGO-Out -->
+                <div class="col-md-offset-9  col-md-1">
+                    <form action="{{route('logout')}}" method="post">
+                        @csrf
+                        <button class="btn btn-primary admin-logout" type="submit">Logout</button>
+                    </form>
+                    {{-- <a href="logout.php" class="" >logout</a> --}}
+                </div>
+                <!-- /LOGO-Out -->
             </div>
         </div>
-        <!-- /HEADER -->
-        <!-- Menu Bar -->
-        <div id="admin-menubar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                       <ul class="admin-menu">
+    </div>
+    <!-- /HEADER -->
+    <!-- Menu Bar -->
+    <div id="admin-menubar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="admin-menu">
                         <li>
                             <a href="/">Home</a>
                         </li>
                         <li>
                             <a href="/admin/posts?user=1">{{auth()->user()->name}}'s Posts</a>
                         </li>
-                            @auth
-                                @if (auth()->user()->role === 1)
-                            <li>
-                                <a href="/admin/posts">All Posts</a>
-                            </li>
-                            <li>
-                                <a href="/admin/category">Category</a>
-                            </li>
-                            <li>
-                                <a href="/admin/users">Users</a>
-                            </li>
-                                @endif 
-                            @endauth
-                        </ul>
-                    </div>
+                        @auth
+                        @if (auth()->user()->role === 1)
+                        <li>
+                            <a href="/admin/posts">All Posts</a>
+                        </li>
+                        <li>
+                            <a href="/admin/category">Category</a>
+                        </li>
+                        <li>
+                            <a href="/admin/users">Users</a>
+                        </li>
+                        @endif
+                        @endauth
+                    </ul>
                 </div>
             </div>
         </div>
-        <!-- /Menu Bar -->
+    </div>
+    <!-- /Menu Bar -->
 
-        @yield('admin-content')
-                
-        <!-- Footer -->
-        <div id ="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <span>© Copyright {{date('Y')}} Lv-blog</span>
-                    </div>
+    @yield('admin-content')
+
+    <!-- Footer -->
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>© Copyright {{date('Y')}} CBS</span>
                 </div>
             </div>
         </div>
-        <!-- /Footer -->
-        </body>
-        </html>
+    </div>
+    <!-- /Footer -->
+</body>
+
+</html>
