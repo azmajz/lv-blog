8e4fc38561c0ddcfaa2d9d771e4efc975a7002b4
azmajz <53705217+azmajz@users.noreply.github.com>
2021-07-29 13:18:58 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{config('app.name')}}</title>
-        <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}" />
-        <link rel="stylesheet" href="{{url('css/font-awesome.css')}}">
-        <link rel="stylesheet" href="{{url('css/style.css')}}">
+        <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/bootstrap.min.css')) }}" />
+        <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/font-awesome.css')) }}" />
+        <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/style.css')) }}" />
 </head>
 <body>
 <div id="header">
