5f7b8b81bbb7d0ec8d5d62b06488f7a70b0cef3e
azmajz <53705217+azmajz@users.noreply.github.com>
2021-07-29 12:38:23 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url(getenv("DATABASE_URL"));
 return [
 
     /*
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,14 +46,14 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => getenv('DB_HOST', '127.0.0.1'),
-            'port' => getenv('DB_PORT', '3306'),
-            'database' => getenv('DB_DATABASE', 'forge'),
-            'username' => getenv('DB_USERNAME', 'forge'),
-            'password' => getenv('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-//             'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
@@ -65,12 +65,18 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+//             'url' => env('DATABASE_URL'),
+//             'host' => env('DB_HOST', '127.0.0.1'),
+//             'port' => env('DB_PORT', '5432'),
+//             'database' => env('DB_DATABASE', 'forge'),
+//             'username' => env('DB_USERNAME', 'forge'),
+//             'password' => env('DB_PASSWORD', ''),
+//             'url' => $DATABASE_URL['host'],
+            'host'     => $DATABASE_URL["host"],
+            'port'     => $DATABASE_URL["port"],
+            'database' => substr($DATABASE_URL["path"], 1),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
