608fa0a90f49be4b05245ed51072b4387b0d3b59
azmajz <53705217+azmajz@users.noreply.github.com>
2021-07-29 13:24:32 +0530

    Update register.blade.php

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -4,9 +4,9 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>ADMIN {{config('app.name')}}</title>
-            <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
-            <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
-            <link rel="stylesheet" href="{{asset('css/style.css')}}">
+            <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/bootstrap.min.css')) }}" />
+            <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/font-awesome.css')) }}" />
+            <link rel="stylesheet" href="{{ str_replace('http://','https://',url('css/style.css')) }}" />
     </head>
     <body>
         <div id="wrapper-admin" class="body-content">
